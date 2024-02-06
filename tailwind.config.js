56e32b7d165798a6a5601ab9761acf8e55adb629
Damy Metzke <damy.metzke@gmail.com>
2024-02-06 12:20:13 +0100

    Style table for time report
    Style table to be more like a proper application table.

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,27 @@ module.exports = {
             positive: "#27ae60",
             visited: "#7f8c8d",
           },
-        }
+        },
+        view: {
+          background: {
+            alternate: "#eff0f1",
+            normal: "#fcfcfc",
+          },
+          decoration: {
+            focus: "#3daee9",
+            hover: "#93cee9",
+          },
+          foreground: {
+            active: "#3daee9",
+            inactive: "#7f8c8d",
+            link: "#2980b9",
+            negative: "#da4453",
+            neutral: "#f67400",
+            normal: "#232629",
+            positive: "#27ae60",
+            visited: "#7f8c8d",
+          },
+        },
       }
     },
   },
