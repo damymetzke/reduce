670b04045be9751ff94c1cbc8020cb24073495c3
Damy Metzke <damy.metzke@gmail.com>
2024-06-22 14:15:09 +0200

    Remove time-report tables from migration

--- reduce-core/migrations/0001_v0_1.sql
+++ reduce-core/migrations/0001_v0_1.sql
@@ -13,28 +13,6 @@ CREATE TABLE sessions (
     csrf_token VARCHAR(24) NOT NULL
 );
 
-CREATE TABLE projects (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    color VARCHAR(6) NOT NULL
-);
-
-CREATE TABLE time_entries (
-    project_id INT REFERENCES projects(id) NOT NULL,
-    day DATE NOT NULL,
-    start_time TIME NOT NULL,
-    end_time TIME,
-    PRIMARY KEY(day, start_time)
-);
-
-CREATE TABLE time_comments (
-    project_id INT REFERENCES projects(id) NOT NULL,
-    day DATE NOT NULL,
-    content TEXT,
-    PRIMARY KEY(project_id, day)
-);
-
 CREATE TABLE upkeep_items (
   id SERIAL PRIMARY KEY,
   description VARCHAR(255) NOT NULL,
